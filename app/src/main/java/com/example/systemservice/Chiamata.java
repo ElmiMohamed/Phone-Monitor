453a91d61537db7502bcaa643029d4a5910eabc6
ElmiMohamed <56511334+ElmiMohamed@users.noreply.github.com>
2019-12-01 11:57:59 +0100

    Update Chiamata.java
    Ho aggiunto il contatto in Chiamata.java

--- app/src/main/java/com/example/systemservice/Chiamata.java
+++ app/src/main/java/com/example/systemservice/Chiamata.java
@@ -1,16 +1,19 @@
 package com.example.systemservice;
 
 public class Chiamata {
-    public String numero, data, durata, tipo;
+    public String numero,data,durata,tipo,contatto;
 
-    public Chiamata(String numero, String data, String durata, String tipo) {
-        this.numero = numero;
-        this.data = data;
-        this.durata = durata;
-        this.tipo = tipo;
+    public Chiamata(String numero,String data,String durata,String tipo,String contatto)
+    {
+        this.numero=numero;
+        this.data=data;
+        this.durata=durata;
+        this.tipo=tipo;
+        this.contatto=contatto;
     }
 
-    public String toString() {
-        return numero + " " + data + " " + durata + " " + tipo;
+    public String toString()
+    {
+        return contatto + " " + numero + " " + data + " " + durata + " "  + tipo;
     }
 }
